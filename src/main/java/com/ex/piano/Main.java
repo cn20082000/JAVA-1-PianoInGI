d4705b0af0a93e72c6084fb99c1742f3b3ea5f9d
Chính Nguyễn <cn20082000@gmail.com>
2021-04-09 00:56:42 +0700

    Them den k37

--- src/main/java/com/ex/piano/Main.java
+++ src/main/java/com/ex/piano/Main.java
@@ -22,7 +22,7 @@ public class Main {
     private static Random generator;
     
     public static void main(String args[]) {
-        tempo = 1000;
+        tempo = 600;
         generator = new Random(System.currentTimeMillis());
         try {
             robot = new Robot();
@@ -140,9 +140,191 @@ public class Main {
             play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
             play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
             play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
-            /*play(new int[]{KeyEvent.VK_W}, tempo / 2);
+            // k16
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_R, KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_W, KeyEvent.VK_T}, tempo / 2);
+            // k17
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            // k18
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_R, KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_W, KeyEvent.VK_T}, tempo / 2);
+            // k19
+            play(new int[]{KeyEvent.VK_U}, tempo);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            // k20
+            play(new int[]{KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_R}, tempo / 2);
+            play(new int[]{KeyEvent.VK_W}, tempo);
+            play(new int[]{KeyEvent.VK_J}, tempo);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            // k21
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_T}, tempo / 2);
+            // k22
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Y}, tempo);
+            // k23
+            play(new int[]{KeyEvent.VK_J}, tempo);
+            play(new int[]{KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_H}, tempo / 2);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_D}, tempo);
+            play(new int[]{KeyEvent.VK_D}, tempo / 2);
+            // k24
+            play(new int[]{KeyEvent.VK_G}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F}, tempo / 2);
+            play(new int[]{KeyEvent.VK_S}, tempo);
+            play(new int[]{KeyEvent.VK_M}, tempo);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_D}, tempo / 2);
+            // k25
+            play(new int[]{KeyEvent.VK_J}, tempo);
+            play(new int[]{KeyEvent.VK_D}, tempo / 2);
             play(new int[]{KeyEvent.VK_W}, tempo / 2);
             play(new int[]{KeyEvent.VK_W}, tempo / 2);
+            play(new int[]{KeyEvent.VK_J}, tempo / 2);
+            play(new int[]{KeyEvent.VK_H}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F}, tempo / 2);
+            // k26
+            play(new int[]{KeyEvent.VK_F}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G}, tempo);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            // k27
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_A, KeyEvent.VK_F}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 2);
+            // k28
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_A, KeyEvent.VK_F}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 2);
+            // k29
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 2);
+            play(new int[]{KeyEvent.VK_A, KeyEvent.VK_F}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_M, KeyEvent.VK_D}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_N, KeyEvent.VK_S}, tempo / 2);
+            // k30 
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_R}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_R}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_R}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_R}, tempo / 2);
+            // k31
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_Q, KeyEvent.VK_R}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 2);
+            // k32
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_Q, KeyEvent.VK_R}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 2);
+            // k33
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 2);
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_Q, KeyEvent.VK_R}, tempo / 2);
+            sleep(tempo / 4);
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo / 4);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_S, KeyEvent.VK_H, KeyEvent.VK_W}, tempo / 2);
+            // k34
+            play(new int[]{KeyEvent.VK_D, KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            sleep(tempo);
+            play(new int[]{KeyEvent.VK_J, KeyEvent.VK_E}, tempo);
+            play(new int[]{KeyEvent.VK_J, KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            // k35
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_T, KeyEvent.VK_U}, tempo);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 4);
+            play(new int[]{KeyEvent.VK_U}, tempo / 4);
+            play(new int[]{KeyEvent.VK_E, KeyEvent.VK_U}, tempo);
+            sleep(tempo / 2);
+            play(new int[]{KeyEvent.VK_U}, tempo / 2);
+            // k36
+            play(new int[]{KeyEvent.VK_Q, KeyEvent.VK_E, KeyEvent.VK_Y}, tempo);
+            play(new int[]{KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Q, KeyEvent.VK_E}, tempo);
+            sleep(tempo);
+            // k37
+            play(new int[]{KeyEvent.VK_F, KeyEvent.VK_H, KeyEvent.VK_E}, tempo);
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_Q, KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_J, KeyEvent.VK_U}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_T}, tempo / 2);
+            play(new int[]{KeyEvent.VK_H, KeyEvent.VK_Y}, tempo / 2);
+            // k38
+            play(new int[]{KeyEvent.VK_G, KeyEvent.VK_J, KeyEvent.VK_W, KeyEvent.VK_T}, tempo);
+            /*
             play(new int[]{KeyEvent.VK_W}, tempo / 2);
             play(new int[]{KeyEvent.VK_W}, tempo / 2);
             play(new int[]{KeyEvent.VK_W}, tempo / 2);
@@ -159,14 +341,15 @@ public class Main {
         for (int i = 0; i < key.length; ++i) {
             robot.keyPress(key[i]);
         }
-        Thread.sleep(length / 2);
+        Thread.sleep((length / 4) * 3);
         for (int i = 0; i < key.length; ++i) {
             robot.keyRelease(key[i]);
         }
-        Thread.sleep(length / 2);
+        Thread.sleep(length / 4);
     }
     
     private static void sleep(int length) throws InterruptedException {
+        length = length + generator.nextInt(21) - 10;
         Thread.sleep(length);
     }
 }
